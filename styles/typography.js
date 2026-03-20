e68bee889348283c10459efc6f937920a5374db6
parthmunjal07 <parth.munjal07@gmail.com>
2026-03-20 13:56:35 +0530

    feat: add styles to typography

--- styles/typography.js
+++ styles/typography.js
@@ -1,6 +1,15 @@
 const typography = {
-    'neo-text-sm': {property: 'font-size', value: '0.875rem'},
-    'neo-text-md': {property: 'font-size', value: '1.750rem'},
-    'neo-text-lg': {property: 'font-size', value: '3.5rem'},
-    'neo-text-xl': {property: 'font-size', value: '0.875rem'},
+    'neo-text-sm': {property: 'fontSize', value: '0.875rem'},
+    'neo-text-md': {property: 'fontSize', value: '1.750rem'},
+    'neo-text-lg': {property: 'fontSize', value: '2.5rem'},
+    'neo-text-xl': {property: 'fontSize', value: '3.5rem'},
+
+    'neo-font-bold': {property: 'fontWeight', value: '700'},
+    'neo-font-smb': {property: 'fontWeight', value: '550'},
+    'neo-font-norm': {property: 'fontWeight', value: '400'},
+    'neo-font-light': {property: 'fontWeight', value: '250'},
+
+    'neo-text-left': {property: 'textAlign', value:'left'},
+    'neo-text-center': {property: 'textAlign', value:'center'},
+    'neo-text-right': {property: 'textAlign', value:'right'},
 }
