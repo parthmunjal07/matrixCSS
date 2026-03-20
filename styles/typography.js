72132be3bf324758edaf60633509a04e508a50aa
parthmunjal07 <parth.munjal07@gmail.com>
2026-03-20 13:47:33 +0530

    first commit

---
+++ styles/typography.js
@@ -0,0 +1,6 @@
+const typography = {
+    'neo-text-sm': {property: 'font-size', value: '0.875rem'},
+    'neo-text-md': {property: 'font-size', value: '1.750rem'},
+    'neo-text-lg': {property: 'font-size', value: '3.5rem'},
+    'neo-text-xl': {property: 'font-size', value: '0.875rem'},
+}
